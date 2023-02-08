ef3cb931f52302595aa6237c24b99ea9b639dd26
moon <moonjh9392@gmail.com>
2023-02-08 23:05:55 +0900

    hotel

--- programmers/Hotel.js
+++ programmers/Hotel.js
@@ -8,7 +8,11 @@ function solution(book_time) {
   //시간을 분으로 생각
   //하루 00:00 ~ 23:59 = 0 ~ 1439(23*60+59)
   const room = [];
-  book_time.map((time) => {
+
+  if (book_time.length === 0) return 0;
+
+  //분단위로 변경
+  const newBookTime = book_time.map((time) => {
     const [start, end] = time;
     const newStart = start.split(':');
     const newEnd = end.split(':');
@@ -16,12 +20,35 @@ function solution(book_time) {
     const totalStart = Number(newStart[0]) * 60 + Number(newStart[1]);
     const totalEnd = Number(newEnd[0]) * 60 + Number(newEnd[1]);
 
-    if (room.length === 0) {
+    return [totalStart, totalEnd];
+  });
+  //입실시간 앞쪽부터 정렬
+  newBookTime.sort((a, b) => a[0] - b[0]);
+
+  newBookTime.map((time) => {
+    const [start, end] = time;
+    checkRoom(room, 0, start, end);
+  });
+
+  return room.length;
+}
+
+function checkRoom(room, index, start, end) {
+  //index 배열이 없으면 새로만듬
+  if (!room[index]) {
+    room.push(new Array(1439).fill(0));
+  }
+  //입실~퇴실까지 배열을 0 => 1로 변경
+  if (room[index][start] === 0) {
+    console.log(start, end);
+    for (let i = start; i <= end; i++) {
+      room[index][i] = 1;
+    }
+  } else {
+    //이미 차있는 방일 경우 새로운 방생성
+    if (!room[index + 1]) {
       room.push(new Array(1439).fill(0));
     }
-
-    if (room[0][totalStart] === 0) {
-    }
-  });
-  return room;
+    checkRoom(room, index + 1, start, end);
+  }
 }
