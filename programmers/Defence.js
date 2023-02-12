0387d6b6cc3f889c3386b6d4a0cbb7bebfe53278
moon <moonjh9392@gmail.com>
2023-02-12 21:05:17 +0900

    defence

--- programmers/Defence.js
+++ programmers/Defence.js
@@ -3,3 +3,23 @@
  * 디펜스 게임
  * Lv 2
  */
+
+function solution(n, k, enemy) {
+  var answer = 0;
+
+  enemy.map((round) => {
+    if (n < round) {
+      if (k > 0) {
+        k -= 1;
+        answer++;
+      }
+    } else {
+      if (k > 0) {
+        n -= round;
+        answer++;
+      }
+    }
+  });
+
+  return answer;
+}
