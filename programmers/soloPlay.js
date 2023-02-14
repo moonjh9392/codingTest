c24c9677335aee691d43c2e722aba99087e77c05
moon <moonjh9392@gmail.com>
2023-02-14 23:38:23 +0900

    soloplay

--- programmers/soloPlay.js
+++ programmers/soloPlay.js
@@ -5,22 +5,25 @@
  */
 
 function solution(cards) {
-  const check = new Array(cards.length).fill(false);
-
-  function open(number) {
-    if (cards[number]) {
-      console.log(number);
-      check[number] = true;
-      open(cards[number] - 1);
+  let answer = 0;
+  const calc = (idx, cards, prev) => {
+    const card = cards[idx];
+    cards[idx] = 0;
+    if (card === 0) return prev;
+    return calc(card - 1, cards, prev + 1);
+  };
+  cards.forEach((card) => {
+    if (card !== 0) {
+      const copy1 = [...cards];
+      const first = calc(card - 1, copy1, 0);
+      copy1.forEach((card) => {
+        if (card !== 0) {
+          const copy2 = [...copy1];
+          const second = calc(card - 1, copy2, 0);
+          answer = Math.max(answer, first * second);
+        }
+      });
     }
-  }
-
-  open(0);
-
-  const group1 = check.filter((e) => e === true).length;
-  const group2 = check.length - group1;
-
-  console.log(group1, group2);
-
-  return group1 * group2;
+  });
+  return answer;
 }
